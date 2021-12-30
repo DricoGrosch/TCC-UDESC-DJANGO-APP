7700fc6a6d9a83ce2f8a7db2c7541b0d35eb9cfb
Adrian Grosch <adrian.grosch@singular.inf.br>
2021-12-30 14:11:39 -0300

    static root

--- backend/settings.py
+++ backend/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "deployment", "backend", "collected_static")
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 
